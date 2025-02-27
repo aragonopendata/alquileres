2ba1ef94c7c4512e97f2c1bd6a74508a5988c6d1
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-02-27 12:05:25 +0100

    Change layout to separate components

--- aod-fianzas/src/app/main/layout/layout.component.ts
+++ aod-fianzas/src/app/main/layout/layout.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
-import { WFSResponse } from 'src/app/shared/models/wfs-response.model';
-import { NgIf } from '@angular/common';
-import { ListsearchComponent } from '../listsearch/listsearch.component';
-import { HeaderComponent } from '../header/header.component';
-import { MapComponent } from '../map/map.component';
+// import { WFSResponse } from 'src/app/shared/models/wfs-response.model';
+// import { NgIf } from '@angular/common';
+// import { ListsearchComponent } from '../listsearch/listsearch.component';
+// import { HeaderComponent } from '../header/header.component';
+// import { MapComponent } from '../map/map.component';
 import { FooterComponent } from '../footer/footer.component';
+import { MapSearchComponent
 
+ } from '../map-search/map-search.component';
+import { ListsearchComponent } from "../listsearch/listsearch.component";
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss'],
-    imports: [NgIf, ListsearchComponent, HeaderComponent, MapComponent, FooterComponent]
+    imports: [MapSearchComponent, FooterComponent, ListsearchComponent]
 })
 export class LayoutComponent {
-  wfsResponse!: WFSResponse;
+  // wfsResponse!: WFSResponse;
   showListSearch = false;
 
-  updateMap(wfsResponse: WFSResponse): void {
-    this.wfsResponse = wfsResponse;
-  }
+  // updateMap(wfsResponse: WFSResponse): void {
+  //   this.wfsResponse = wfsResponse;
+  // }
 
   toggleComponents(): void {
     this.showListSearch = !this.showListSearch;
---
+++ aod-fianzas/src/app/main/map-search/map-search.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapSearchComponent } from './map-search.component';
+
+describe('MapSearchComponent', () => {
+  let component: MapSearchComponent;
+  let fixture: ComponentFixture<MapSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MapSearchComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MapSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
