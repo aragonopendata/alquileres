14ecc267ce17527a210ebccbaade6eb39ce67ee4
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-03-27 10:43:56 +0100

    Fix imports in main

--- aod-fianzas-back/main.py
+++ aod-fianzas-back/main.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-from database import query_municipalities, query_streets_by_municipality, query_fianzas_by_municipality_street, \
+from database import query_municipalities, query_streets_by_municipality, \
     query_stats_by_street_and_municipality, DBException
 
 app = FastAPI()
@@ -29,12 +29,6 @@ def get_municipalities():
 def get_streets_by_municipality(municipality: str):
     streets = query_streets_by_municipality(municipality)
     return streets
-
-
-@app.get("/municipality/{municipality}/street/{street}")
-def get_fianzas_by_municipality_street(municipality: str, street: str):
-    fianzas = query_fianzas_by_municipality_street(municipality, street)
-    return fianzas
 
 @app.get("/municipality/{municipality}/street/{street}/stats")
 def get_stats_by_municipality_street(municipality: str, street: str):
