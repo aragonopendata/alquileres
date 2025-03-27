8f007d14902687b74a35b68d2f5b5c9b425d7781
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-03-27 10:32:31 +0100

    Fix street selection when municipality changes.

--- aod-fianzas/src/app/main/listsearch/listsearch.component.ts
+++ aod-fianzas/src/app/main/listsearch/listsearch.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { FianzaListComponent } from '../fianza-list/fianza-list.component';
 import { AlquileresApiService } from 'src/app/shared/services/alquileres-api.service';
+import { FormsModule } from '@angular/forms';
+
+interface  Municipality {
+  nombre_municipio: string;
+}
+
+interface Street {
+  nombre_calle: string;
+}
+
 
 @Component({
     selector: 'app-listsearch',
     templateUrl: './listsearch.component.html',
     styleUrls: ['./listsearch.component.scss'],
-    imports: [NgFor, FianzaListComponent]
+    imports: [NgFor, FianzaListComponent, FormsModule]
 })
 export class ListsearchComponent implements OnInit {
 
-  municipalities: any[] = [];
-  streets: any[] = [];
+  municipalities: Municipality[] = [];
+  streets: Street[] = [];
   selectedMunicipality = '';
   selectedStreet = '';
 
@@ -21,14 +31,16 @@ export class ListsearchComponent implements OnInit {
   constructor(private alquileresService: AlquileresApiService) { }
 
   ngOnInit(): void {
-    this.fetchMunicipalities()
+    this.fetchMunicipalities();
   }
 
   fetchMunicipalities(): void {
     this.alquileresService.fetchMunicipalities().subscribe(data => {
       this.municipalities = data;
     },
-      error => {console.error('Error fetching municipalities from api. ', error)});
+      error => {
+      console.error('Error fetching municipalities from api. ', error);
+    });
   }
 
 
@@ -36,21 +48,27 @@ export class ListsearchComponent implements OnInit {
     this.alquileresService.fetchStreets(municipality).subscribe(data => {
       this.streets = data;
     },
-      error => {console.error('Error fetching streets from api. ', error)});
+      error => {
+      console.error('Error fetching streets from api. ', error);
+    });
   }
 
   onMunicipalityChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const municipality = selectElement.value;
-    this.selectedMunicipality =municipality
-    this.fetchStreets(municipality)
+    this.selectedMunicipality = municipality;
+
+    this.selectedStreet = '';
+    this.streets = [];
+    this.fetchStreets(municipality);
+    console.log(this.selectedStreet);
   }
 
   onStreetChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const selectedStreet = selectElement.value;
-    this.selectedStreet = selectedStreet
+    this.selectedStreet = selectedStreet;
     this.selectionChanged.emit({ municipality: this.selectedMunicipality, street: selectedStreet });
-    console.log("Emitting {this.selectedMunicipality}{selectedStreet}.")
+    console.log('Emitting {this.selectedMunicipality}{selectedStreet}.');
   }
 }
