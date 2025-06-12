5bd1a4f8d164ab05f1e4a043b5de68f70b0eb0cf
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-06-12 15:29:51 +0200

    Update layout styles and navigation links in the main component

--- aod-fianzas/tailwind.config.js
+++ aod-fianzas/tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.{html,ts}']
-  },
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
